c7bbf3c042317b49682e2a0700ef28d3d421275c
prabakaran.r <prabakaran.r@zohocorp.com>
2023-09-13 11:15:53 +0530

    Save callback sample implementation added

---
+++ save-callback-implementation/express-js/README.md
@@ -0,0 +1,27 @@
+Save the [code](https://github.com/iampraba/zoi-php-sdk-examples/blob/main/save_callback_implementation/express-js/save_callback.js) in a file, for example, save_callback.js.
+
+To run the following commands from your Mac terminal, you should have nodejs installed on you machine. 
+
+Open the terminal, navigate to the directory save-callback-implementation, and run the following command:
+
+ ``` sh
+npm install
+  ```
+
+  ``` sh
+node save_callback.js
+  ```
+
+This will start the api end point to receive file. You can access the API endpoint at http://localhost:3000/zoho/file/uploader.
+
+Test above api end point and verify if the file store without any error. You could free to edit the code to fit your application needs. 
+
+To host this code on a server so that it can be accessed from the internet, you would need a web server with nodejs support. Here are the general steps:
+
+- Choose a hosting provider or set up your own server with a web server software like Apache or Nginx.
+- Upload the your save_callback.js file to your server.
+- Make sure the server has nodejs installed and properly configured.
+- Ensure that the destination folder for uploaded files has appropriate write permissions.
+- Access the API endpoint using the server's domain or IP address, e.g., http://zylker.com//zoho/file/uploader.
+
+Note that the exact steps may vary depending on your hosting environment and configuration. It's recommended to refer to the documentation provided by your hosting provider or the server software you choose for more detailed instructions.  
---
+++ save-callback-implementation/express-js/save_callback.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+//To get absolute file path to writer file read from post request
+const { resolve } = require('path');
+
+const app = express();
+const upload = multer();
+
+// Start the server
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`\nServer listening on port ${port}`);
+
+  console.log(`\nMake a post request to this end point to test save callback from local file - http://localhost/${port}${callbackEndpoint}`);
+
+  console.log(`\nYou should not use http://localhost/${port} as save_url in callback settings. You should only use it as publicly accessible(with your application domain) end point.`);
+});
+
+var showCallback = async (req, res) => {
+  try {
+    // multer middleware will read the file and store it to file key in req object
+    const fileData = req.file;
+    const outputFilePath = resolve(".") + "/" + fileData.originalname;
+
+    //Writing file to localdisk. Use can write the file in any of your storage service. Modify the code accordingly
+    fs.writeFileSync( outputFilePath, fileData.buffer); 
+    console.log('\nFile uploaded successfully in file path - ' + outputFilePath);
+
+    // Send a response back to Zoho API to acknowledge the callback successfully completed.
+    // String value given for message key below will shown as save status in editor UI.
+    res.status(200).json({ message: "Document Save Successfully in NodeJS Server."});
+  } catch (err) {
+    console.error('Error in saving file.', err);
+
+    // You can send an appropriate error response back to Zoho Show API here if needed.
+    // String value given for message key below will shown as save status in editor UI.
+    res.status(500).json({ message: 'Error in saving file' });
+  }
+};
+
+//saveurl callback api end point
+const callbackEndpoint = "/zoho/file/uploader";
+
+app.post(callbackEndpoint, upload.single('content'), showCallback);
